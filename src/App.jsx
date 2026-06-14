67e332dda491a73debf6a65ec98c1be6789f0efe
JarrinBryan <ferjj828@gmail.com>
2026-06-13 21:06:14 -0500

    nuevos elementos

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,29 @@
 import "./styles/App.css";
 import Timer from "./components/Timer";
-import Controls from "./components/Controls";
 
 function App() {
   return (
     <main className="app">
-      <section className="pomodoro-card">
-        <h1>FocusFlow</h1>
+      <div className="dashboard">
 
-        <Timer />
+        <header className="header">
+          <h1>FocusFlow</h1>
+          <p>Organiza tu tiempo y tareas</p>
+        </header>
 
-        <Controls />
-      </section>
+        <section className="content">
+
+          <div className="left-panel">
+            <Timer />
+          </div>
+
+          <div className="right-panel">
+            <h2>Tareas de Hoy</h2>
+          </div>
+
+        </section>
+
+      </div>
     </main>
   );
 }
